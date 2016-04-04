4e966c8ce1a039ab43ddef31bd6b4ab76f6d7a0e
yukieen <lpczclt@gmail.com>
2016-04-04 13:32:50 +0900

    add main class

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -1,7 +1,14 @@
 package sample;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by yukieen on 2016/04/04.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
